4c1d78de990f23f7a1f3623855f45bed40ec130a
jaishankerpillai84 <JaiPillai@Jaishankers-MacBook-Pro.local>
2015-05-23 17:17:16 -0700

    Webservice for quiz application

---
+++ QuizService/src/main/java/com/jaypillai/pojo/QuizData.java
@@ -0,0 +1,55 @@
+package com.jaypillai.pojo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+public class QuizData {
+	public QuizData() {}
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	private String quizName;
+	private String question;
+	private ArrayList<String> choices;
+	private String answer;
+	public String getQuizName() {
+		return quizName;
+	}
+	public void setQuizName(String quizName) {
+		this.quizName = quizName;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	public ArrayList<String> getChoices() {
+		return choices;
+	}
+	public void setChoices(ArrayList<String> choices) {
+		this.choices = choices;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+}
---
+++ QuizService/src/main/java/com/jaypillai/quizapp/Services.java
@@ -0,0 +1,108 @@
+package com.jaypillai.quizapp;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.jaypillai.pojo.LoginData;
+import com.jaypillai.pojo.QuizData;
+
+@Path("/quizapp/*")
+public class Services {
+	private SessionFactory sessionFactory;
+	public Services() {
+		sessionFactory = new Configuration().configure().buildSessionFactory();
+	}
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String DefaultMessage() {
+		return "Welcome to quiz app.";
+	}
+	@Path("authenticate_user/")
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String AuthenticateUser(@QueryParam("username") String username,
+			@QueryParam("password") String password) {
+		System.out.println("Inside AuthenticateUser with username:" + username + " and password:" + password);
+		Session session = sessionFactory.openSession();
+		Query query = session.createQuery("FROM LoginData where Username=:q");
+		query.setParameter("q", username);
+		List<Object> results = query.list();
+		System.out.println("Number of results obtained is " + results.size());
+		boolean authenticate = false;
+		for(Object result : results) {
+			LoginData data = (LoginData)result;
+			System.out.println("Result is username:" + data.getUsername() + " password:" + data.getPassword());
+			if(data.getPassword().equals(password)) {
+				authenticate = true;
+			}
+		}
+		System.out.println("Authentication is " + authenticate);
+		return authenticate ? "true" : "false";
+	}
+	@Path("/get_quiz_list")
+	@GET
+	@Produces("application/json")
+	public List<String> GetQuizList() {
+		System.out.println("Inside GetQuizList");
+		Session session = sessionFactory.openSession();
+		Query query = session.createQuery("SELECT distinct quizName FROM QuizData");
+		List<String> list = query.list();
+		for(String q : list) {
+			System.out.println(q);
+		}
+		return list;
+	}
+	@Path("add_question")
+	@GET
+	@Produces("application/json")
+	public QuizData AddQuestions(@QueryParam("quizname") String quizName) {
+		System.out.println("Adding question to " + quizName);
+		// Generate question.
+		Random rand = new Random();
+		String question = "Question No " + Integer.toString(rand.nextInt());
+		ArrayList<String> choices = new ArrayList<String>();
+		choices.add(Integer.toString(rand.nextInt()));
+		choices.add(Integer.toString(rand.nextInt()));
+		choices.add(Integer.toString(rand.nextInt()));
+		choices.add(Integer.toString(rand.nextInt()));
+		String answer = choices.get(rand.nextInt(4));
+		QuizData newData = new QuizData();
+		newData.setQuestion(question);
+		newData.setAnswer(answer);
+		newData.setChoices(choices);
+		newData.setQuizName(quizName);
+		
+		// Save to database.
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(newData);
+		transaction.commit();
+		session.close();
+		
+		return newData;
+	}
+	@Path("get_questions/")
+	@GET
+	@Produces("application/json")
+	public List<QuizData> GetQuestions(@QueryParam("quizname") String quizName) {
+		System.out.println("Getting questions of "+ quizName);
+		Session session = sessionFactory.openSession();
+		Query query = session.createQuery("FROM QuizData WHERE quizName=:q");
+		query.setParameter("q", quizName);
+		List<QuizData> list = query.list();
+		return list;
+	}
+}
